8e10d6a1a38095cc804a794456c32b4634bf409f
ekta1999 <ektamishra1999@gmail.com>
2019-09-19 10:32:21 +0530

    Files added

--- src/com/ekta/lab2/CountSort.java
+++ src/com/ekta/lab2/CountSort.java
@@ -1,4 +1,16 @@
 package com.ekta.lab2;
 
+import java.util.Scanner;
+
 public class CountSort {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        System.out.println("Enter the size of array : ");
+        int n = s.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the elements : ");
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = s.nextInt();
+        }
+    }
 }
