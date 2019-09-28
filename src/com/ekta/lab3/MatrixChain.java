816efb814b5a0247f11cbe5b5347c316dec59451
ekta1999 <ektamishra1999@gmail.com>
2019-09-28 19:13:52 +0530

    Files added

--- src/com/ekta/lab3/MatrixChain.java
+++ src/com/ekta/lab3/MatrixChain.java
@@ -1,4 +1,13 @@
 package com.ekta.lab3;
 
+import java.util.Scanner;
+
 public class MatrixChain {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the no. of matrix : ");
+        int n = s.nextInt();
+        System.out.println("Enter the dimensions of the matrices : ");
+        int[] dim = new int[n+1];
+    }
 }
