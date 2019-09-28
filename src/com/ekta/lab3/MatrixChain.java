483a9be462237871361057064f2aa2d23e40d2a4
ekta1999 <ektamishra1999@gmail.com>
2019-09-28 19:22:33 +0530

    Files added

--- src/com/ekta/lab3/MatrixChain.java
+++ src/com/ekta/lab3/MatrixChain.java
@@ -5,9 +5,30 @@ import java.util.Scanner;
 public class MatrixChain {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter the no. of matrix : ");
+        System.out.print("Enter the no. of matrix : ");
         int n = s.nextInt();
-        System.out.println("Enter the dimensions of the matrices : ");
+        System.out.print("Enter the dimensions of the matrices : ");
         int[] dim = new int[n+1];
+        for (int i = 0; i < dim.length; i++) {
+            dim[i] = s.nextInt();
+        }
+
+        System.out.println("Minimum no. of multiplication : " + MatrixChainOrder(dim,1,dim.length-1));
+    }
+
+    public static int MatrixChainOrder(int p[], int i, int j)
+    {
+        if (i == j)
+            return 0;
+        int min = Integer.MAX_VALUE;
+        for (int k=i; k<j; k++)
+        {
+            int count = MatrixChainOrder(p, i, k) +
+                    MatrixChainOrder(p, k+1, j) +
+                    p[i-1]*p[k]*p[j];
+            if (count < min)
+                min = count;
+        }
+        return min;
     }
 }
